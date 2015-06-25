77345c957afa23f5200655155672e4fa3db0861e
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2015-06-25 15:27:24 +0200

    sort categories after creating the file

--- bin/5_generate_spherecategories.sh
+++ bin/5_generate_spherecategories.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# sort by parent ID (and then ID?) and hope that that's enough to be able to build the tree (until category import can build the tree itself)
-csvsort -c pcatid,catid ../transformed/category_en.csv > category_en.sorted.csv
-
 # TODO filter categories by "visible" (or enable import to take them as unpublished)
 
 # map categories as SPHERE import wants it.
-csv-mapper -m ../mapping_categories.yaml --inCsv ../transformed/category_en.csv  --outCsv ../generated/categories.4sphere.csv
+csv-mapper -m ../mapping_categories.yaml --inCsv ../transformed/category_en.csv  --outCsv ../transformed/categories.4sphere.unsorted.csv
+
+# sort by parent ID (and then ID?) and hope that that's enough to be able to build the tree (until category import can build the tree itself)
+csvsort -c parentId,externalId ../transformed/categories.4sphere.unsorted.csv > ../transformed/categories.4sphere.csv
 
